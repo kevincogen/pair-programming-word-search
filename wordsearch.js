de65317a3015583c33ce034fc71582923b4c558e
Kevin Cogen <k.cogen@gmail.com>
2023-02-24 19:28:06 -0500

    commit to include vertical search, and returning false when word is not present

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+const horizontalJoin = letters.map(ls => ls.join(''));
+const invertedLetters = [];
+    for (let i = 0; i < letters[0].length; i++) {
+      const colArr = [];
+      for (let j = 0; j < letters.length; j++) {
+        colArr.push(letters[j][i]);
+      }
+    invertedLetters.push(colArr);
     }
-}
+const verticalJoin = invertedLetters.map(cs => cs.join(''));
 
-module.exports = wordSearch
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) {return true}
+    }
+  
+    for (let l of verticalJoin) {
+        if (l.includes(word)) {return true}
+    }
+return false;
+};
+
+module.exports = wordSearch;
